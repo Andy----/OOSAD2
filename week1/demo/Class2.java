c511606300274745ce05363e1bcefc1f6154be32
Andy---- <andypollard84gmail.com>
2015-01-29 12:41:55 +0000

    Added Java files

---
+++ week1/demo/Class2.java
@@ -0,0 +1,40 @@
+package demo;
+
+public class Class2 {
+
+	private string name;
+	private int number;
+
+	public Class2() {
+		// TODO - implement Class2.Class2
+		throw new UnsupportedOperationException();
+	}
+
+	public void getName() {
+		// TODO - implement Class2.getName
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param name
+	 */
+	public void setName(int name) {
+		// TODO - implement Class2.setName
+		throw new UnsupportedOperationException();
+	}
+
+	public void getNumber() {
+		// TODO - implement Class2.getNumber
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param number
+	 */
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+}
---
+++ week1/demo/Example.java
@@ -0,0 +1,40 @@
+package demo;
+
+public class Example {
+
+	private string name;
+	private int number;
+
+	public Example() {
+		// TODO - implement Example.Example
+		throw new UnsupportedOperationException();
+	}
+
+	public void getName() {
+		// TODO - implement Example.getName
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param name
+	 */
+	public void setName(int name) {
+		// TODO - implement Example.setName
+		throw new UnsupportedOperationException();
+	}
+
+	public void getNumber() {
+		// TODO - implement Example.getNumber
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param number
+	 */
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+}
